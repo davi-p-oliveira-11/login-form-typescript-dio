1b52ef74c25bcd3da878a96d39fda022fd877642
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-19 14:20:56 -0300

    added login container basic styling pause for lunch

---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginContainer = styled.div`
+  width: 100%;
+  max-width: 420px;
+  background-color: #f5fafc;
+
+  border-radius: 8px;
+  padding: 20px;
+
+  flex: 1;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
