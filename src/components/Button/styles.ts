a4c4848431cd9136334d73b3869a895d08c0e0a4
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-19 19:30:43 -0300

    added button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  width: 100%;
+  height: 42px;
+  background-color: #197aff;
+  color: #f5fafc;
+
+  border: 1px solid #197aff;
+  border-radius: 21px;
+
+  &:hover {
+    opacity: 0.6;
+    cursor: pointer;
+  }
+`;
--- src/pages/Login/App.tsx
+++ src/pages/Login/App.tsx
@@ -1,3 +1,10 @@
+import { useForm } from "react-hook-form";
+
+import Input from "../../components/Input";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import {
   Container,
   LoginContainer,
@@ -8,10 +15,32 @@ import {
   SubTitle,
   MainText,
 } from "./styles";
+import { defaultValues, IFormLogin } from "./types";
 
 import { loginContent } from "../../data/data";
+import Button from "../../components/Button";
+
+const schema = yup
+  .object({
+    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    password: yup
+      .string()
+      .min(6, "No minimo 6 caracteres")
+      .required("Campo obrigatório"),
+  })
+  .required();
 
 const Login = () => {
+  const {
+    control,
+    formState: { error, isValid },
+  } = useForm<IFormLogin>({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+    defaultValues,
+    reValidateMode: "onChange",
+  });
+
   return (
     <Container>
       <LoginContainer>
@@ -21,6 +50,23 @@ const Login = () => {
           <SubTitle>{loginContent.subtitle}</SubTitle>
           <SpacingSm />
           <MainText>{loginContent.maintext}</MainText>
+          <SpacingMd />
+          <Input
+            name="email"
+            placeholder="Email"
+            control={control}
+            errorMessage={error?.email?.message}
+          />
+          <SpacingMd />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Password"
+            control={control}
+            errorMessage={error?.password?.message}
+          />
+          <SpacingMd />
+          <Button title="Enter" />
         </Column>
       </LoginContainer>
     </Container>
